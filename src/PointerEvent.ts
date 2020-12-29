cc125218051db7e697c2f5de268168d7b4ee89f2
Joseph R Miles <joe@jrm.dev>
2020-12-29 08:05:26 -0800

    feat: added pointer event interface

--- src/PointerEvent.ts
+++ src/PointerEvent.ts
@@ -1,4 +1,13 @@
-export default class PointerEvent extends MouseEvent implements PointerEvent {
+/**
+ * The PointerEvent interface as described by W3C:
+ * https://www.w3.org/TR/pointerevents/#pointerevent-interface
+ */
+interface IPointerEvent {
+  pointerId: number;
+
+  width: number;
+}
+
   public pointerId: number;
 
   constructor(type: string, options?: PointerEventInit) {
