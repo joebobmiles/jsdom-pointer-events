8a4b09a0636991a310ab5fad6c11822894fd4de2
Joseph R Miles <joe@jrm.dev>
2021-03-13 13:10:24 -0800

    chore: switching between workstations

---
+++ src/mouse-event-patch.test.ts
@@ -0,0 +1,12 @@
+// import { createEvent, fireEvent, } from "@testing-library/dom";
+import "@testing-library/jest-dom/extend-expect";
+
+import ".";
+
+describe("PointerEvent is sent when", () =>
+{
+  it("Receives a mousedown event.", () =>
+  {
+    expect(true).toBeTruthy();
+  });
+});
